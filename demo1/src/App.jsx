cd60e871138691d1a14721ce827b1a86a401f676
skonstantis <skonstantis@uth.gr>
2024-03-15 17:28:36 +0200

    building the app

--- demo1/src/App.jsx
+++ demo1/src/App.jsx
@@ -1,8 +1,26 @@
+import { useState } from "react";
+
 export default function App() {
- 
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  function handleSubmit(e){
+    e.preventDefault();
+    setTodos([...todos, todo]);
+    setTodo("");
+    console.log(todos);
+  }
+
   return (
     <div className="App">
-      app
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          onChange={(e) => setTodo(e.target.value)}
+          value={todo}
+        />
+        <button type="sumbit">Sumbit</button>
+      </form>
     </div>
-  )
+  );
 }
