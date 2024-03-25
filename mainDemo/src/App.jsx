50a8ae9fd5408814917f9efa67ed9e3edd600e38
skonstantis <skonstantis@uth.gr>
2024-03-25 20:16:21 +0200

    building the app

--- mainDemo/src/App.jsx
+++ mainDemo/src/App.jsx
@@ -1,12 +1,15 @@
 /* Author: Sotiris Konstantis */
 
+import FoodList from "./components/FoodList";
 import Search from "./components/Search";
+import { useState } from "react";
 
 export default function App() {
-
+  const [foodData, setFoodData] = useState([]);
   return (
     <div className="App">
-      <Search/>
+      <Search foodData={foodData} setFoodData={setFoodData} />
+      <FoodList foodData={foodData}/>
     </div>
-  )
+  );
 }
--- mainDemo/src/components/Search.jsx
+++ mainDemo/src/components/Search.jsx
@@ -6,13 +6,13 @@ import { useEffect } from "react";
 const URL = "https://api.spoonacular.com/recipes/complexSearch";
 const apiKey = "6b86e75fd5c041bca2581cdb22283187"; //this is a free api key from spoonacular with no associated billing plan. If still valid, feel free to use for testing of the app.
 
-export default function Search() {
+export default function Search({foodData, setFoodData}) {
   const [query, setQuery] = useState("Pizza");
   useEffect(() => {
     async function fetchSearch() {
       const res = await fetch(`${URL}?query=${query}&apiKey=${apiKey}`);
       const data = await res.json();
-      console.log(data.results);
+      setFoodData(data.results);
     }
     fetchSearch();
   }, [query]);
